2765ce6184bfe4c0c7f11b6fa8deb9278eea2d7f
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-06-04 18:14:43 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+const header = document.querySelector(".header");
+window.addEventListener("scroll", (e) => {
+  if (scrollY >= 50) {
+    header.classList.add("header_function");
+  } else if (scrollY <= 50) {
+    header.classList.remove("header_function");
+  }
+});
